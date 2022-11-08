b26b6321709a3a610d9ba8244667e35247ff0f67
SKProCH <skproch@yandex.ru>
2022-11-08 17:04:45 +0300

    Update to Avalonia 11.0.0-preview3

--- AvaloniaDialogHostBugWithKeyboard.Android/MainActivity.cs
+++ AvaloniaDialogHostBugWithKeyboard.Android/MainActivity.cs
@@ -6,11 +6,6 @@ using Avalonia;
 namespace AvaloniaDialogHostBugWithKeyboard.Android
 {
     [Activity(Label = "AvaloniaDialogHostBugWithKeyboard.Android", Theme = "@style/MyTheme.NoActionBar", Icon = "@drawable/icon", LaunchMode = LaunchMode.SingleInstance, ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize)]
-    public class MainActivity : AvaloniaActivity<App>
-    {
-        protected override AppBuilder CustomizeAppBuilder(AppBuilder builder)
-        {
-            return base.CustomizeAppBuilder(builder);
-        }
-    }
+    public class MainActivity : AvaloniaMainActivity
+    { }
 }
